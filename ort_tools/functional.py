729f7612f127d5dced4a9ad236281ba635f4a8c4
ninja-96 <oikachalov@icloud.com>
2024-05-22 18:21:47 +0000

    add typing for sample input

--- ort_tools/functional.py
+++ ort_tools/functional.py
@@ -3,6 +3,30 @@ This module stores addition functions for project.
 """
 
 from typing import Dict
+import numpy as np
+
+
+__all__ = [
+    'shape_str_decode'
+]
+
+
+_mapper = {
+    'fp16': np.float16,
+    'fp32': np.float32,
+
+    'int8': np.int8,
+    'int16': np.int16,
+    'int32': np.int32,
+    'int64': np.int64,
+
+    'uint8': np.uint8,
+    'uint16': np.uint16,
+    'uint32': np.uint32,
+    'uint64': np.uint64,
+
+    'bool': np.bool_
+}
 
 
 def shape_str_decode(shapes: str) -> Dict:
@@ -21,9 +45,12 @@ def shape_str_decode(shapes: str) -> Dict:
 
     input_cfg = {}
     for input_ in shapes.split(','):
-        name, shape = input_.split(':')
-        input_cfg[name] = []
+        name, shape, dtype = input_.split(':')
+        input_cfg[name] = {}
+        input_cfg[name]['shape'] = []
+        input_cfg[name]['dtype'] = None
         for s_item in shape.split('x'):
-            input_cfg[name].append(int(s_item))
+            input_cfg[name]['shape'].append(int(s_item))
+            input_cfg[name]['dtype'] = _mapper[dtype]
 
     return input_cfg
